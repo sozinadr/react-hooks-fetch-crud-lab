d09c1a36d7ab52edefcf8035bd014d371a5af93f
sozi <sozyarnadir@gmail.com>
2022-10-24 17:26:48 +0300

    Assignment Completed

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,25 +1,41 @@
 import React from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question, onDeleteClick, onAnswerChange }) {
+    const { id, prompt, answers, correctIndex } = question;
 
-  const options = answers.map((answer, index) => (
-    <option key={index} value={index}>
-      {answer}
-    </option>
-  ));
+    const options = answers.map((answer, index) => ( <
+        option key = { index }
+        value = { index } > { answer } <
+        /option>
+    ));
 
-  return (
-    <li>
-      <h4>Question {id}</h4>
-      <h5>Prompt: {prompt}</h5>
-      <label>
+
+    function handleAnswerChange(event) {
+        onAnswerChange(id, parseInt(event.target.value));
+    }
+
+    function handleDeleteClick() {
+        onDeleteClick(id);
+    }
+
+
+
+    return ( <
+        li >
+        <
+        h4 > Question { id } < /h4> <
+        h5 > Prompt: { prompt } < /h5> <
+        label >
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
-      </label>
-      <button>Delete Question</button>
-    </li>
-  );
+        <
+        <
+        select defaultValue = { correctIndex } > { options }
+        onChange = { handleAnswerChange } < /select>
+        label > <
+        <
+        button onClick = { handleDeleteClick } > Delete Question < /button>
+        li >
+    );
 }
 
 export default QuestionItem;
